18169a0e23f7ace3ceacafd801dc3b6b506f8368
leijue222 <1035897872@qq.com>
2022-01-12 21:54:19 +0800

    update

--- lib/dataset/__init__.py
+++ lib/dataset/__init__.py
@@ -9,6 +9,7 @@ from __future__ import division
 from __future__ import print_function
 
 from .mpii import MPIIDataset as mpii
+# from .coco_ochuman import COCODataset as coco
 from .coco import COCODataset as coco
 from .crowdpose import CROWDPOSEDataset as crowdpose
 from .ochuman import OCHumanDataset as OCHuman
